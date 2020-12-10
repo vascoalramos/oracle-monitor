5b442ea9086ad8284f622391eb2f4c3c92769dc9
Vasco Ramos <vascoalramos@outlook.pt>
2020-12-10 00:36:32 +0000

    Update queries

--- database/data_queries.sql
+++ database/data_queries.sql
@@ -11,24 +11,21 @@ select T as "TOTAL (MB)", U as "USED (MB)"
     ) "B" on dual.DUMMY = "B".DUMMY
     ;
 
--- query for default tablespaces     
-select df.tablespace_name,
-    df.totalspace total,
-    (df.totalspace - tu.totalusedspace) free,
-    totalusedspace used,
-    round(100 * ( (df.totalspace - tu.totalusedspace)/ df.totalspace)) percentage_free,
-    100 - round(100 * ( (df.totalspace - tu.totalusedspace)/ df.totalspace)) percentage_used
-from
-    (select tablespace_name,
-        round(sum(bytes) / 1048576) TotalSpace
-     from dba_data_files
-     group by tablespace_name) df,
-        (select round(sum(bytes)/(1024*1024)) totalusedspace, tablespace_name
-         from dba_segments
-         group by tablespace_name) tu
-where df.tablespace_name = tu.tablespace_name;
-    
--- query for temp tablespaces
+-- query for tablespaces
+select b.tablespace_name,
+    tbs_size total,
+    a.free_space free,
+    round(tbs_size - a.free_space) used,
+    round(100 * ( (tbs_size - round(tbs_size - a.free_space))/ tbs_size)) percentage_free,
+    100 - round(100 * ( (tbs_size - round(tbs_size - a.free_space))/ tbs_size)) percentage_used
+from  (select tablespace_name, round(sum(bytes)/1024/1024) as free_space
+       from dba_free_space
+       group by tablespace_name) a,
+      (select tablespace_name, sum(bytes)/1024/1024 as tbs_size
+       from dba_data_files
+       group by tablespace_name) b
+where a.tablespace_name(+)=b.tablespace_name
+UNION ALL
 SELECT tablespace_name,
     round(tablespace_size / (1024*1024)) total,
     round(free_space / (1024*1024)) free,
@@ -38,8 +35,9 @@ SELECT tablespace_name,
 FROM dba_temp_free_space;
          
 
--- query for default datafiles
-select d.tablespace_name,
+-- query for datafiles
+select tablespace_name, file_name, total, free, used, percentage_used, percentage_free
+    from (select d.tablespace_name,
     d.file_id,
     d.file_name,
     round(d.bytes / (1024*1024)) total,
@@ -53,10 +51,8 @@ from (select tablespace_name, file_id, file_name, sum(bytes) bytes from dba_data
         group by tablespace_name, file_id) f
 where d.tablespace_name = f.tablespace_name(+)
     and d.file_id = f.file_id(+)
-order by tablespace_name, file_id;
-
-
--- query for temp datafiles
+order by tablespace_name, file_id)
+union ALL
 select d.tablespace_name,
     d.file_name,
     round(d.bytes / (1024*1024)) total,
