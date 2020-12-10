6f5f2b2888c4eb3c4fa8c938eb38310618421ab3
Carolina Marques <carolinaresendemarques@ua.pt>
2020-12-10 21:50:25 +0000

    Agent and Queries in python being developed

--- scrapper-agent/agent.py
+++ scrapper-agent/agent.py
@@ -1,6 +1,6 @@
 import cx_Oracle
 import config
-from sql_queries import pdb_sql
+from sql_queries import pdb_sql,sessions_sql, memory_sql, tablespaces_sql, datafiles_sql
 
 batch_size = 20
 
@@ -24,7 +24,6 @@ def pdb_query():
 
 def insert_pdb_entries(pdb_rows):
     sql = "insert into pdb_history(name, con_id, total_size, tstp) values(:name, :con_id, :total_size, :tstp)"
-
     with cx_Oracle.connect(
         config.username2, config.password2, config.dsn2, encoding=config.encoding
     ) as connection:
@@ -34,8 +33,148 @@ def insert_pdb_entries(pdb_rows):
             connection.commit()
 
 
-try:
-    pdb_query()
+def pdb_query_session():
+    with cx_Oracle.connect(
+        config.username,
+        config.password,
+        config.dsn,
+        cx_Oracle.SYSDBA,
+        encoding=config.encoding,
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(sessions_sql)
+            while True:
+                rows = cursor.fetchmany(batch_size)
+                if not rows:
+                    break
+                insert_session_entries(rows)
 
+
+def insert_session_entries(pdb_rows):
+    sql = "insert into session_history(sid, con_id, username, status, program, type, tstp) values(:sid, :con_id, :username, :status, :program, :type, :tstp)"
+    with cx_Oracle.connect(
+        config.username2, config.password2, config.dsn2, encoding=config.encoding
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.setinputsizes(None, None, None, None, None, None, cx_Oracle.TIMESTAMP)
+            print(pdb_rows)
+            cursor.executemany(sql, pdb_rows)
+            connection.commit()
+
+def pdb_query_memory():
+    with cx_Oracle.connect(
+        config.username,
+        config.password,
+        config.dsn,
+        cx_Oracle.SYSDBA,
+        encoding=config.encoding,
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(memory_sql)
+            while True:
+                rows = cursor.fetchmany(batch_size)
+                if not rows:
+                    break
+                insert_session_entries(rows)
+
+
+def insert_memory_entries(pdb_rows):
+    sql = "insert into memory_history(total,used,tstp) values(:total,:used,:tstp)"
+    with cx_Oracle.connect(
+        config.username2, config.password2, config.dsn2, encoding=config.encoding
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.setinputsizes(None, None, cx_Oracle.TIMESTAMP)
+            print (pdb_rows)
+            cursor.executemany(sql, pdb_rows)
+            connection.commit()
+
+def pdb_query_tablespaces():
+    with cx_Oracle.connect(
+        config.username,
+        config.password,
+        config.dsn,
+        cx_Oracle.SYSDBA,
+        encoding=config.encoding,
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(tablespaces_sql)
+            while True:
+                rows = cursor.fetchmany(batch_size)
+                if not rows:
+                    break
+                insert_tablespaces_entries(rows)
+
+
+def insert_tablespaces_entries(pdb_rows):
+    sql = "insert into tablespace_history(name, total, free, used, percentage_free, percentage_used,tstp) values(:tablespace_name,:total,:used,:percentage_free,:percentage_used,:tstp)"
+    with cx_Oracle.connect(
+        config.username2, config.password2, config.dsn2, encoding=config.encoding
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.setinputsizes(None, None, None,None, None, cx_Oracle.TIMESTAMP)
+            cursor.executemany(sql, pdb_rows)
+            connection.commit()
+
+def pdb_query_datafiles():
+    with cx_Oracle.connect(
+        config.username,
+        config.password,
+        config.dsn,
+        cx_Oracle.SYSDBA,
+        encoding=config.encoding,
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(datafiles_sql)
+            while True:
+                rows = cursor.fetchmany(batch_size)
+                if not rows:
+                    break
+                insert_datafiles_entries(rows)
+
+
+def insert_datafiles_entries(pdb_rows):
+    sql = "insert into datafile_history(tablespace_name, datafile_name, total, free, used, percentage_free, percentage_used,tstp) values(:tablespace_name,:file_name,:total,:free,:used,:percentage_free,:percentage_used,:tstp)"
+    with cx_Oracle.connect(
+        config.username2, config.password2, config.dsn2, encoding=config.encoding
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.setinputsizes(None, None, None,None, None, None, None, cx_Oracle.TIMESTAMP)
+            cursor.executemany(sql, pdb_rows)
+            connection.commit()
+
+def pdb_query_users():
+    with cx_Oracle.connect(
+        config.username,
+        config.password,
+        config.dsn,
+        cx_Oracle.SYSDBA,
+        encoding=config.encoding,
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(datafiles_sql)
+            while True:
+                rows = cursor.fetchmany(batch_size)
+                if not rows:
+                    break
+                insert_entry_entries(rows)
+
+
+def insert_users_entries(pdb_rows):
+    sql = "insert into datafile_history(tablespace_name, datafile_name, total, free, used, percentage_free, percentage_used,tstp) values(:tablespace_name,:file_name,:total,:free,:used,:percentage_free,:percentage_used,:tstp)"
+
+    with cx_Oracle.connect(
+        config.username2, config.password2, config.dsn2, encoding=config.encoding
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.setinputsizes(None, None, None, None, None, None,None, cx_Oracle.TIMESTAMP)
+            cursor.executemany(sql, pdb_rows)
+            connection.commit()
+
+try:
+   # pdb_query()
+   # pdb_query_session()
+    pdb_query_memory()
+   # pdb_query_tablespaces()
 except cx_Oracle.Error as error:
     print("Error occurred: " + error)
--- scrapper-agent/sql_queries.py
+++ scrapper-agent/sql_queries.py
@@ -1,1 +1,74 @@
 pdb_sql = "select name, con_id, round(total_size / (1024*1024*1024), 2) as total_size_gb, CURRENT_TIMESTAMP from V$PDBS"
+
+sessions_sql = "select sid, con_id, username, status, program, 'type', CURRENT_TIMESTAMP from V$SESSION"
+
+memory_sql = '''select T as total, U as used, CURRENT_TIMESTAMP
+    from dual
+    inner join (
+        select sum(pga_max_mem)/1024/1024 "U", 'X' "DUMMY"
+        from v$process
+    ) "A" on dual.DUMMY = "A".DUMMY
+    inner join (
+        select  sum(value)/1024/1024 "T", 'X' "DUMMY"
+        from v$sga
+    ) "B" on dual.DUMMY = "B".DUMMY'''
+
+tablespaces_sql = '''select b.tablespace_name,
+    tbs_size total,
+    a.free_space free,
+    round(tbs_size - a.free_space) used,
+    round(100 * ( (tbs_size - round(tbs_size - a.free_space))/ tbs_size)) percentage_free,
+    100 - round(100 * ( (tbs_size - round(tbs_size - a.free_space))/ tbs_size)) percentage_used,
+    CURRENT_TIMESTAMP
+    from  (select tablespace_name, round(sum(bytes)/1024/1024) as free_space
+       from dba_free_space
+       group by tablespace_name) a,
+      (select tablespace_name, sum(bytes)/1024/1024 as tbs_size
+       from dba_data_files
+       group by tablespace_name) b
+    where a.tablespace_name(+)=b.tablespace_name
+    UNION ALL
+    SELECT tablespace_name,
+       round(tablespace_size / (1024*1024)) total,
+       round(free_space / (1024*1024)) free,
+       round((tablespace_size - free_space) / (1024*1024)) used,
+       100 - round(((tablespace_size / (1024*1024)) - (free_space / (1024*1024))) / (tablespace_size / (1024*1024)) * 100) percentage_free,
+       round(((tablespace_size / (1024*1024)) - (free_space / (1024*1024))) / (tablespace_size / (1024*1024)) * 100) percentage_used,
+        CURRENT_TIMESTAMP
+    FROM dba_temp_free_space'''
+
+datafiles_sql = '''select tablespace_name, file_name, total, free, used, percentage_used, percentage_free, CURRENT_TIMESTAMP
+    from (select d.tablespace_name,
+	    d.file_id,
+	    d.file_name,
+	    round(d.bytes / (1024*1024)) total,
+	    round(nvl(f.bytes,0) / (1024*1024)) free,
+	    round((d.bytes - nvl(f.bytes,0)) / (1024*1024)) used,
+	    round(((d.bytes - nvl(f.bytes,0)) / d.bytes) * 100) percentage_used,
+	    100 - round(((d.bytes - nvl(f.bytes,0)) / d.bytes) * 100) percentage_free,
+	    current_timestamp
+	from (select tablespace_name, file_id, file_name, sum(bytes) bytes from dba_data_files
+	    group by tablespace_name, file_id, file_name) d,
+	    (select tablespace_name, file_id, sum(bytes) bytes from dba_free_space
+		group by tablespace_name, file_id) f
+	where d.tablespace_name = f.tablespace_name(+)
+	    and d.file_id = f.file_id(+)
+	order by tablespace_name, file_id)
+	union ALL
+	select d.tablespace_name,
+	    d.file_name,
+	    round(d.bytes / (1024*1024)) total,
+	    round(nvl(f.bytes,0) / (1024*1024)) free,
+	    round((d.bytes - nvl(f.bytes,0)) / (1024*1024)) used,
+	    round(((d.bytes - nvl(f.bytes,0)) / d.bytes) * 100) percentage_used,
+	    100 - round(((d.bytes - nvl(f.bytes,0)) / d.bytes) * 100) percentage_free,
+	    current_timestamp
+	from (select tablespace_name, file_name, sum(bytes) bytes from dba_temp_files
+	    group by tablespace_name, file_name) d,
+	    (select tablespace_name, allocated_space bytes from dba_temp_free_space
+		group by tablespace_name, allocated_space) f
+	where d.tablespace_name = f.tablespace_name(+)
+	order by tablespace_name'''
+
+users_sql = '''select user_id, username, account_status, default_tablespace, temporary_tablespace, last_login, CURRENT_TIMESTAMP from dba_users'''
+
