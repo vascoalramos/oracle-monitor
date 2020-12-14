1e0cf030f64289557bd39d5da799ed04d32edb1a
Vasco Ramos <vascoalramos@outlook.pt>
2020-12-14 20:58:21 +0000

    Add grouping by datafiles

--- rest-api/routes/datafile.js
+++ rest-api/routes/datafile.js
@@ -31,12 +31,32 @@ router.get("/", (req, res) => {
 router.get("/history", (req, res) => {
     let final_data = {};
 
-    if ("tablespace" in req.query) {
-        let tablespace = req.query.tablespace;
-        controller
-            .filter(tablespace)
-            .then((data) => {
-                final_data["entities"] = data;
+    let tablespace_exists = "tablespace" in req.query;
+
+    let tablespace = req.query.tablespace;
+
+    controller
+        .filter(tablespace)
+        .then((data) => {
+            final_data["entities"] = data;
+            let groupBy_exists = "groupBy" in req.query;
+
+            if (tablespace_exists && groupBy_exists) {
+                let groupBy = req.query.groupBy;
+                if (groupBy_belongs(groupBy)) {
+                    controller
+                        .group_filtered_history(groupBy, tablespace)
+                        .then((data) => {
+                            final_data["history"] = data;
+                            res.status(200).jsonp(final_data);
+                        })
+                        .catch((error) => {
+                            res.status(500).jsonp(error);
+                        });
+                } else {
+                    res.status(400).jsonp({ error: "Invalid parameters" });
+                }
+            } else if (tablespace_exists) {
                 controller
                     .filter_history(tablespace)
                     .then((data) => {
@@ -46,29 +66,48 @@ router.get("/history", (req, res) => {
                     .catch((error) => {
                         res.status(500).jsonp(error);
                     });
-            })
-            .catch((error) => {
-                res.status(500).jsonp(error);
-            });
-    } else {
-        controller
-            .list()
-            .then((data) => {
-                final_data["entities"] = data;
+            } else if (groupBy_exists) {
+                let groupBy = req.query.groupBy;
+                if (groupBy_belongs(groupBy)) {
+                    controller
+                        .group_history(groupBy)
+                        .then((data) => {
+                            final_data["history"] = data;
+                            res.status(200).jsonp(final_data);
+                        })
+                        .catch((error) => {
+                            res.status(500).jsonp(error);
+                        });
+                } else {
+                    res.status(400).jsonp({ error: "Invalid parameters" });
+                }
+            } else {
                 controller
-                    .list_history()
+                    .list()
                     .then((data) => {
-                        final_data["history"] = data;
-                        res.status(200).jsonp(final_data);
+                        final_data["entities"] = data;
+                        controller
+                            .list_history()
+                            .then((data) => {
+                                final_data["history"] = data;
+                                res.status(200).jsonp(final_data);
+                            })
+                            .catch((error) => {
+                                res.status(500).jsonp(error);
+                            });
                     })
                     .catch((error) => {
                         res.status(500).jsonp(error);
                     });
-            })
-            .catch((error) => {
-                res.status(500).jsonp(error);
-            });
-    }
+            }
+        })
+        .catch((error) => {
+            res.status(500).jsonp(error);
+        });
 });
+
+let groupBy_belongs = (groupBy) => {
+    return ["minute", "hour", "day", "month", "year"].includes(groupBy);
+};
 
 module.exports = router;
