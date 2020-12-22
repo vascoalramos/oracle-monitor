e6de8a450c185d6b17f748e07b1499cdfb4fd6ac
Vasco Ramos <vascoalramos@outlook.pt>
2020-12-22 23:16:24 +0000

    Add pdbs and cpus max

--- rest-api/controllers/pdb.js
+++ rest-api/controllers/pdb.js
@@ -5,21 +5,31 @@ module.exports.list = () => {
 };
 
 module.exports.list_history = () => {
-    return db.execute(`SELECT
-                            NAME as "name",
-                            CON_ID as "con_id",
-                            TOTAL_SIZE as "size",
-                            TSTP as "tstp"
-                       FROM pdb_values
-                       ORDER BY TSTP`);
+    return db.execute(`SELECT *
+                       FROM (
+                             SELECT
+                                NAME as "name",
+                                CON_ID as "con_id",
+                                TOTAL_SIZE as "size",
+                                TSTP as "tstp"
+                             FROM pdb_values
+                             ORDER BY TSTP DESC
+                            )
+                        WHERE rownum <= (select count(distinct name) from pdb_values) * 30
+                       ORDER BY rownum DESC`);
 };
 
 module.exports.group_history = (time) => {
-    return db.execute(`SELECT
-                            NAME as "name",
-                            CON_ID as "con_id",
-                            TOTAL_SIZE as "size",
-                            TSTP as "tstp"
-                       FROM view_pdb_values_per_${time}
-                       ORDER BY TSTP`);
+    return db.execute(`SELECT *
+                       FROM (
+                             SELECT
+                                NAME as "name",
+                                CON_ID as "con_id",
+                                TOTAL_SIZE as "size",
+                                TSTP as "tstp"
+                             FROM view_pdb_values_per_${time}
+                             ORDER BY TSTP DESC
+                            )
+                        WHERE rownum <= (select count(distinct name) from view_pdb_values_per_${time}) * 30
+                       ORDER BY rownum DESC`);
 };
